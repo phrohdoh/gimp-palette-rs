8cdae5b7bf6bf80df3003215bf74a1d6840e7039
Taryn Hill <taryn@phrohdoh.com>
2017-05-04 08:34:00 -0500

    Implement gimp_palette::read_from_file

--- src/bin/gp-cli.rs
+++ src/bin/gp-cli.rs
@@ -1,7 +1,20 @@
 extern crate gimp_palette;
+use gimp_palette::{Palette, NewPaletteError};
 
 fn main() {
-    let colors = vec![ gimp_palette::Color { r: 0, g: 50, b: 255 } ];
-    let palette = gimp_palette::Palette::new("Example", colors).unwrap();
-    palette.write_to_file("test.gpl").expect(&format!("Failed to write {} to test.gpl", palette.get_name()));
+    match std::env::args().nth(1) {
+        None => println!("Please provide a path to a gpl file"),
+        Some(file_path) => {
+            let pal = match Palette::read_from_file(&file_path) {
+                Ok(p) => p,
+                Err(e) => match e {
+                    NewPaletteError::NoColors => unreachable!(),
+                    NewPaletteError::InvalidData { line_num, val } => panic!("Line {} has invalid data: {}", line_num, val),
+                    NewPaletteError::IoErr(io_err) => panic!("{}", io_err),
+                }
+            };
+
+            println!("Found {} color(s) in {}", pal.get_colors().len(), file_path);
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
+use std::fs::File;
 
 pub struct Color {
     pub r: u8,
@@ -12,18 +13,119 @@ pub struct Palette {
     _colors: Vec<Color>,
 }
 
+pub enum NewPaletteError {
+    NoColors,
+    InvalidData { line_num: usize, val: String },
+    IoErr(std::io::Error),
+}
+
+impl From<std::io::Error> for NewPaletteError {
+    fn from(e: std::io::Error) -> Self {
+        NewPaletteError::IoErr(e)
+    }
+}
+
 impl Palette {
     /// Creates a new named Palette from a non-zero-length collection of Colors.
     ///
-    /// Passing a zero-length collection will result in an Err(String)
-    pub fn new<S: ToString>(name: S, colors: Vec<Color>) -> Result<Self, String> {
+    /// Passing a zero-length collection will result in an Err(NewPaletteError::NoColors)
+    pub fn new<S: ToString>(name: S, colors: Vec<Color>) -> Result<Self, NewPaletteError> {
         match colors.len() {
-            0 => Err(String::from("Palettes must have at least 1 color")),
-            _ => Ok(Palette {
-                _name: name.to_string(),
-                _colors: colors,
-            }),
+            0 => Err(NewPaletteError::NoColors),
+            _ => {
+                Ok(Palette {
+                       _name: name.to_string(),
+                       _colors: colors,
+                   })
+            }
         }
+    }
+
+    pub fn read_from_file<P: AsRef<Path>>(file_path: P) -> Result<Self, NewPaletteError> {
+        let f = File::open(&file_path)?;
+        let reader = BufReader::new(f);
+
+        fn is_comment(s: &str) -> bool {
+            s.chars().skip_while(|c| c.is_whitespace()).next() == Some('#')
+        }
+
+        let mut colors = vec![];
+        let mut name = String::new();
+        //let mut color_count = 0;
+
+        let mut lines = reader.lines().enumerate();
+        while let Some((index, Ok(line))) = lines.next() {
+            if is_comment(&line) || line.trim().len() == 0 {
+                continue;
+            }
+
+            let line_num = index + 1;
+
+            if line_num == 1 {
+                if line != "GIMP Palette" {
+                    return Err(NewPaletteError::InvalidData {
+                                   line_num,
+                                   val: line,
+                               });
+                }
+
+                continue;
+            } else if line_num == 2 {
+                if !line.starts_with("Name:") {
+                    return Err(NewPaletteError::InvalidData {
+                                   line_num,
+                                   val: line,
+                               });
+                }
+
+                name = line[4..].trim().to_string();
+                continue;
+            } else if line_num == 3 {
+                if !line.starts_with("Columns:") {
+                    return Err(NewPaletteError::InvalidData {
+                                   line_num,
+                                   val: line,
+                               });
+                }
+
+                // TODO: Handle this value.
+                // - Should we only read `color_count` colors, or
+                // - Err if color_count != (line count - 3 - comments), or
+                // - Explicitly ignore this value
+                //color_count = line[4..].trim().to_string();
+                continue;
+            }
+
+            let mut split = line.split_whitespace();
+            match (split.next(), split.next(), split.next()) {
+                (Some(r_str), Some(g_str), Some(b_str)) => {
+                    let r =
+                        r_str
+                            .parse::<u8>()
+                            .expect(&format!("Failed to parse line {}'s r into a byte", line_num));
+                    let g =
+                        g_str
+                            .parse::<u8>()
+                            .expect(&format!("Failed to parse line {}'s g into a byte", line_num));
+                    let b =
+                        b_str
+                            .parse::<u8>()
+                            .expect(&format!("Failed to parse line {}'s b into a byte", line_num));
+                    colors.push(Color { r, g, b });
+                }
+                _ => {
+                    return Err(NewPaletteError::InvalidData {
+                                   line_num,
+                                   val: line.clone(),
+                               })
+                }
+            }
+        }
+
+        Ok(Self {
+               _name: name,
+               _colors: colors,
+           })
     }
 
     pub fn get_name<'a>(&'a self) -> &'a str {
@@ -35,7 +137,9 @@ impl Palette {
     }
 
     pub fn write_to_file<P: AsRef<Path>>(&self, file_path: P) -> Result<(), std::io::Error> {
-        let header = format!("GIMP Palette\nName: {}\nColumns: {}\n", self._name, self._colors.len());
+        let header = format!("GIMP Palette\nName: {}\nColumns: {}\n",
+                             self._name,
+                             self._colors.len());
         let colors_string = create_string_from_colors(&self._colors);
         let mut f = std::fs::OpenOptions::new()
             .write(true)
@@ -54,7 +158,10 @@ impl ToString for Color {
 }
 
 fn create_string_from_colors(colors: &[Color]) -> String {
-    colors.iter().map(|c| c.to_string() + "\n").collect::<String>()
+    colors
+        .iter()
+        .map(|c| c.to_string() + "\n")
+        .collect::<String>()
 }
 
 #[cfg(test)]
@@ -68,8 +175,7 @@ mod tests {
     fn colors_1() {
         let colors = vec![super::Color { r: 255, g: 0, b: 0 }];
 
-        assert_eq!(super::create_string_from_colors(&colors),
-                   "255   0   0\n");
+        assert_eq!(super::create_string_from_colors(&colors), "255   0   0\n");
     }
 
     #[test]
